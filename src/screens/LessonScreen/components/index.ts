56d132450dfa50ab3bfda12850549627fa77d3eb
Leon Kwan <leonkwan46@gmail.com>
2025-09-10 11:00:22 +0100

    refactor: remove QuestionDisplay component and update imports
    - Remove unused QuestionDisplay.tsx component
    - Update component exports in index.ts
    - Update Description component styling and implementation
    - Add documentation and git hooks setup scripts

--- src/screens/LessonScreen/components/index.ts
+++ src/screens/LessonScreen/components/index.ts
@@ -1,6 +1,3 @@
 // Export all lesson screen components
 export { AnswerInterface } from './AnswerInterface'
-export { MusicStaff } from './MusicStaff'
-export { QuestionDisplay } from './QuestionDisplay'
 export { VisualQuestion } from './VisualQuestion'
-
