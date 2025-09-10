4a67ad1c64bb3b9b7f6f1607f1eee83eceffdfc1
Leon Kwan <leonkwan46@gmail.com>
2025-09-10 11:29:05 +0100

    fix typescript error

--- src/screens/LessonScreen/components/AnswerInterface/AnswerInterface.styles.tsx
+++ src/screens/LessonScreen/components/AnswerInterface/AnswerInterface.styles.tsx
@@ -1,12 +1,11 @@
 import styled from '@emotion/native'
-import { useDevice } from '@/hooks'
 
 export const AnswerInterfaceContainer = styled.View<{ isTablet: boolean }>(({ theme, isTablet }) => ({
   padding: isTablet ? theme.spacing.xl : theme.spacing.lg,
   backgroundColor: theme.colors.surface,
   borderRadius: theme.spacing.sm,
   width: isTablet ? '95%' : '100%',
-  maxWidth: isTablet ? '600px' : 'none',
+  maxWidth: isTablet ? 600 : undefined,
   alignSelf: 'center'
 }))
 
