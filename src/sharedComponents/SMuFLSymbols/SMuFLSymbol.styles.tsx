0957d5ecbb48389f2c1fe2e52b635dba5f301517
Leon Kwan <leonkwan46@gmail.com>
2025-09-27 11:32:27 +0100

    Improve musical terms exercise functionality

--- src/sharedComponents/SMuFLSymbols/SMuFLSymbol.styles.tsx
+++ src/sharedComponents/SMuFLSymbols/SMuFLSymbol.styles.tsx
@@ -1,11 +1,17 @@
 import styled from '@emotion/native'
 import { scale } from 'react-native-size-matters'
 
-export const SMuFLSymbolContainer = styled.Text<{ isTablet?: boolean }>(({ theme, isTablet }) => ({
-  fontFamily: 'Bravura',
-  fontSize: isTablet ? scale(24) : scale(40),
+export const SMuFLSymbolContainer = styled.Text<{ isTablet?: boolean; isTempoText?: boolean }>(({ theme, isTablet, isTempoText }) => ({
+  fontFamily: isTempoText ? 'Times New Roman' : 'Bravura',
+  fontSize: isTempoText 
+    ? (isTablet ? scale(18) : scale(24)) 
+    : (isTablet ? scale(24) : scale(40)),
   color: theme.colors.text,
   textAlign: 'center',
   textAlignVertical: 'center',
-  margin: isTablet ? -25 : -20
+  margin: isTempoText 
+    ? (isTablet ? scale(8) : scale(12)) 
+    : (isTablet ? -25 : -20),
+  fontStyle: isTempoText ? 'italic' : 'normal',
+  fontWeight: isTempoText ? '500' : 'normal'
 }))
