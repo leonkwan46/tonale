2ac7c2f69eb88c0fe10d84bc65fc3ef3c277a123
Leon Kwan <leonkwan46@gmail.com>
2025-10-11 12:25:51 +0100

    Fix Firebase config and move google-services.json to correct location

--- src/config/firebase/firebase.ts
+++ src/config/firebase/firebase.ts
@@ -7,8 +7,7 @@ const firebaseConfig = {
   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 }
 
 export const app = initializeApp(firebaseConfig)
