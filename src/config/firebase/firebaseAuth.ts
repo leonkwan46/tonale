d9f6abe5a4661e2b1af81cc5549f100a7da40ad5
Leon Kwan <leonkwan46@gmail.com>
2025-10-11 21:30:33 +0100

    feat: connect to Firebase emulators in development
    - Add emulator connections for Auth, Firestore, and Functions
    - Connect to localhost emulators when __DEV__ is true
    - Add functions export from firebase.ts
    - Configure Auth emulator on port 9099
    - Configure Firestore emulator on port 8080
    - Configure Functions emulator on port 5001

--- src/config/firebase/firebaseAuth.ts
+++ src/config/firebase/firebaseAuth.ts
@@ -1,4 +1,9 @@
-import { getAuth } from 'firebase/auth'
+import { connectAuthEmulator, getAuth } from 'firebase/auth'
 import { app } from './firebase'
 
 export const auth = getAuth(app)
+
+// Connect to Auth emulator in development
+if (__DEV__) {
+  connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true })
+}
