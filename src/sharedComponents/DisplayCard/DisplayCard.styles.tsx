a213f1fd2c8cb835a4d6d3970a0ea74086f60b79
Leon Kwan <leonkwan46@gmail.com>
2025-10-16 23:49:50 +0100

    Merge branch 'staging' into improve-lesson

--- src/sharedComponents/DisplayCard/DisplayCard.styles.tsx
+++ src/sharedComponents/DisplayCard/DisplayCard.styles.tsx
@@ -1,4 +1,4 @@
-import styled from '@emotion/native';
+import styled from '@emotion/native'
 
 export const DisplayCardContainer = styled.View<{ isTablet: boolean; extraHeight?: boolean }>(({ theme, isTablet, extraHeight }) => ({
   backgroundColor: '#ffffff',
