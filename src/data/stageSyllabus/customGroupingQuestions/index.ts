1b9b50a802a38a1e19e64de1248417adc2e48d8b
Leon Kwan <leonkwan46@gmail.com>
2025-11-26 23:58:53 +0000

    refactor: remove old src/data folder structure
    This removes the old data folder organization that has been replaced
    by the new src/theory/ structure. The old structure included:
    - exerciseGenerators/ (moved to theory/exercises/generators/)
    - helpers/ (moved to theory/utils/)
    - questionGeneration/ (moved to theory/generate.ts)
    - stageSyllabus/ (moved to theory/exercises/custom/ and theory/curriculum/config/)
    - theoryData/ (moved to theory/curriculum/)
    
    All functionality has been migrated to the new structure.

--- src/data/stageSyllabus/customGroupingQuestions/index.ts
+++
@@ -1,5 +0,0 @@
-import { FOUR_FOUR_QUESTIONS } from './fourFourQuestions'
-import { THREE_FOUR_QUESTIONS } from './threeFourQuestions'
-import { TWO_FOUR_QUESTIONS } from './twoFourQuestions'
-
-export { FOUR_FOUR_QUESTIONS, THREE_FOUR_QUESTIONS, TWO_FOUR_QUESTIONS }
