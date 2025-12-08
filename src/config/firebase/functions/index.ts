71e6e67f1c5c5cdc361983c611e38f12f59fd866
Leon Kwan <leonkwan46@gmail.com>
2025-12-08 23:46:30 +0000

    refactor: update firebase functions config
    - Update function imports and exports

--- src/config/firebase/functions/index.ts
+++ src/config/firebase/functions/index.ts
@@ -3,3 +3,6 @@ export { createUserData, deleteUserData, getUserData, updateUserData, type UserP
 
 // Export lesson progress functions
 export * from './lessonProgress'
+
+// Export wrong questions functions
+export * from './wrongQuestions'
