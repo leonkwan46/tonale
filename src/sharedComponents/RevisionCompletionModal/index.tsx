1e1001fa4c21e7ba960b3a34ef49f5b78bc61b62
Leon Kwan <leonkwan46@gmail.com>
2025-12-11 13:50:51 +0000

    feat: add revision completion modal component
    - Add modal to show revision completion status
    - Display remaining questions count
    - Provide options to exit or continue revising
    - Inline type definitions in component file

---
+++ src/sharedComponents/RevisionCompletionModal/index.tsx
@@ -0,0 +1,79 @@
+import { useDevice } from '@/hooks'
+import * as React from 'react'
+import {
+  ButtonContainer,
+  DescriptionText,
+  ModalButton,
+  ModalButtonText,
+  ModalContainer,
+  ModalOverlay,
+  TitleText
+} from './RevisionCompletionModal.styles'
+interface RevisionCompletionModalProps {
+  remainingQuestions: number
+  onExit: () => void
+  onRevise: () => void
+}
+
+export const RevisionCompletionModal: React.FC<RevisionCompletionModalProps> = ({
+  remainingQuestions,
+  onExit,
+  onRevise
+}) => {
+  const { isTablet } = useDevice()
+  const isAllComplete = remainingQuestions === 0
+
+  return (
+    <ModalOverlay>
+      <ModalContainer isTablet={isTablet}>
+        <TitleText isTablet={isTablet}>
+          {isAllComplete ? 'Congratulations!' : 'Revision Complete!'}
+        </TitleText>
+        <DescriptionText isTablet={isTablet}>
+          {isAllComplete
+            ? 'You\'ve completed all your revision questions! Great job!'
+            : `You have ${remainingQuestions} question${remainingQuestions !== 1 ? 's' : ''} left to revise.`}
+        </DescriptionText>
+        <ButtonContainer isTablet={isTablet} singleButton={isAllComplete}>
+          {isAllComplete ? (
+            <ModalButton
+              testID="okay-button"
+              variant="filled"
+              isTablet={isTablet}
+              singleButton={true}
+              onPress={onExit}
+            >
+              <ModalButtonText variant="filled" isTablet={isTablet}>
+                Done!
+              </ModalButtonText>
+            </ModalButton>
+          ) : (
+            <>
+              <ModalButton
+                testID="exit-button"
+                variant="outlined"
+                isTablet={isTablet}
+                onPress={onExit}
+              >
+                <ModalButtonText variant="outlined" isTablet={isTablet}>
+                  Exit
+                </ModalButtonText>
+              </ModalButton>
+              <ModalButton
+                testID="revise-button"
+                variant="filled"
+                isTablet={isTablet}
+                onPress={onRevise}
+              >
+                <ModalButtonText variant="filled" isTablet={isTablet}>
+                  Revise
+                </ModalButtonText>
+              </ModalButton>
+            </>
+          )}
+        </ButtonContainer>
+      </ModalContainer>
+    </ModalOverlay>
+  )
+}
+
