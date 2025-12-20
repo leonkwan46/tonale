750eb44424ab06018e74cbc71197d04d1525e8f1
Leon Kwan <leonkwan46@gmail.com>
2025-12-20 12:23:33 +0000

    Export useMicrophonePermission from hooks index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,7 @@
 export { useLastLesson } from '../screens/HomeScreen/components/LessonCard/useLastLesson'
 export { useDevice, useResponsiveValue } from './useDevice'
 export { useMetronome } from './useMetronome'
+export { useMicrophonePermission } from './useMicrophonePermission'
 export { usePlayer } from './usePlayer'
 export { usePreventDoubleTap } from './usePreventDoubleTap'
 export { useProgress } from './useProgressContext'
