a8106d8b5765e437bef0482b0a40911ec516ec37
Leon Kwan <leonkwan46@gmail.com>
2026-01-18 12:02:00 +0000

    refactor: remove re-export file from AuthScreen components
    - Delete components/index.ts re-export file
    - Follow .cursorrules: Never use re-exports - import directly from source files

--- src/screens/AuthScreen/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { AppLogo } from './AppLogo'
-export { AuthForm } from './AuthForm'
-export { GuestLogin } from './GuestLogin'
-export { Header } from './Header'
-export { ModeToggle } from './ModeToggle'
-
