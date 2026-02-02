d34b211a6da67465837e8e2181ab62bbe0923d9f
Leon Kwan <leonkwan46@gmail.com>
2026-02-02 12:39:00 +0000

    feat: add FinalTestSuccessModal with congratulations message
    - Create modal component for successful final test completion
    - Use Button3D for Continue button with finalTest color variant

---
+++ src/screens/LessonScreen/components/FinalTestSuccessModal/index.tsx
@@ -0,0 +1,54 @@
+import { Button3D } from '@/sharedComponents/Button3D'
+import { Modal } from '@/sharedComponents/Modal'
+import {
+    ButtonContainer,
+    DescriptionText,
+    ModalContainer,
+    ModalOverlay
+} from '@/sharedComponents/Modal/Modal.styles'
+import { ButtonContent, ButtonText, SuccessIcon, SuccessTitleText } from './FinalTestSuccessModal.styles'
+
+interface FinalTestSuccessModalProps {
+  visible: boolean
+  onContinue: () => void
+}
+
+export const FinalTestSuccessModal = ({
+  visible,
+  onContinue
+}: FinalTestSuccessModalProps) => {
+  return (
+    <Modal visible={visible} onRequestClose={onContinue}>
+      <ModalOverlay>
+        <ModalContainer>
+          <SuccessIcon>🎉</SuccessIcon>
+          
+          <SuccessTitleText>
+            Congratulations!
+          </SuccessTitleText>
+
+          <DescriptionText>
+            You&apos;ve passed the final test! Great work on mastering this stage.
+          </DescriptionText>
+          
+          <ButtonContainer singleButton={true}>
+            <Button3D
+              testID="continue-button"
+              color="blue"
+              fullWidth
+              onPress={onContinue}
+            >
+              {() => (
+                <ButtonContent>
+                  <ButtonText>
+                    Continue
+                  </ButtonText>
+                </ButtonContent>
+              )}
+            </Button3D>
+          </ButtonContainer>
+        </ModalContainer>
+      </ModalOverlay>
+    </Modal>
+  )
+}
